00dcd5f118c16dc41215b141d1673ab93aad49c5
RTpushpa <Pushpa@rututechnologies.com>
2018-03-22 17:15:52 +0530

    new campaingn,campaign details and all tracking data are complete

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ Route::group(['namespace'=>'V1','middleware'=>['web','auth']], function () {
     Route::get('/campaignP', ['uses' => 'CampaignController@check_phone_number']);
     Route::get('/campaignGoal', ['uses' => 'CampaignController@goal_action']);
     Route::get('/newCampaign', ['uses' => 'CampaignController@new_compaign']);
-    Route::get('/viewCampaign', ['uses' => 'CampaignController@view_compaign']);
+    Route::any('/viewCampaign', ['uses' => 'CampaignController@view_compaign_Details']);
     Route::get('/Mycampaign', ['uses' => 'mycampaignController@Mycampaign']);
+
+    //Route::any('/viewCampaign', ['uses' => 'CampaignController@all_Tacking_data']);
+    
+    //Route::get('/viewCampaign', ['uses' => 'mycampaignController@allTrackngData']);
+    Route::post('/new_Campaign_General', ['uses' => 'CampaignController@new_Campaign_General']);
+    Route::post('/save_Goal_Action', ['uses' => 'CampaignController@save_Goal_Action']);
 });
 	//Manage users routes
    	Route::get('/usersList', ['uses' => 'UserController@usersList'])->name('UsersList');
