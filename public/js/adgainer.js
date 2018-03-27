939e54071fb0883c1b33c03f1e87300946a0be7f
RTChetan <Chetan@rututechnologies.com>
2018-03-27 20:27:50 +0800

    Preparing environment

---
+++ public/js/adgainer.js
@@ -0,0 +1,90 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+eval("(function ($) {\n    $(function () {});\n})(jQuery);//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9yZXNvdXJjZXMvYXNzZXRzL2pzL2FkZ2FpbmVyLmpzPzMzM2YiXSwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCQpe1xuICAgICQoZnVuY3Rpb24oICl7XG4gICAgICAgIFxuICAgICAgICBcbiAgICB9KTtcbn0oalF1ZXJ5KSk7XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIHJlc291cmNlcy9hc3NldHMvanMvYWRnYWluZXIuanMiXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFJQSIsInNvdXJjZVJvb3QiOiIifQ==");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3Jlc291cmNlcy9hc3NldHMvc2Fzcy9hZGdhaW5lci5zY3NzP2M3ZjYiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL3Jlc291cmNlcy9hc3NldHMvc2Fzcy9hZGdhaW5lci5zY3NzXG4vLyBtb2R1bGUgaWQgPSAxXG4vLyBtb2R1bGUgY2h1bmtzID0gMCJdLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+module.exports = __webpack_require__(1);
+
+
+/***/ })
+/******/ ]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,107 +1,109 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') </title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title') </title>
 
-    <link rel="icon"  type="icon" href="{!! asset('favicon.ico') !!}">
-    <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
-    <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
-    <link rel="stylesheet" href="{!! asset('css/custom.css') !!}" />
-    <!--<link rel="stylesheet" href="http://tawian.io/text-spinners/spinners.css">-->
-    <link href="{!! asset('css/plugins/footable/footable.core.css') !!}" rel="stylesheet">
-    <link href="{!! asset('css/plugins/dataTables/datatables.min.css') !!}"
-          rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css"
-          rel="stylesheet"/>
-    <link rel="stylesheet" href="{{ asset('custom/css/responsive_style.css')}}">
-<!-- Font Awesome -->
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">  <!--roboto css-->
-    
-    <link rel="stylesheet" href="{{ asset('custom/css/responsive_style.css')}}"> <!--custom css-->
-    <link rel="stylesheet" href="{{ asset('custom/css/animate.css') }}">
-    <!--animate css-->
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet"
-          href="{{ asset('custom/dist/css/skins/_all-skins.min.css') }}">
-    <!-- iCheck -->
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/iCheck/flat/blue.css')}}">
-    <!-- Morris chart -->
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/morris/morris.css')}}">
-    <!-- jvectormap -->
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
-    <!-- Date Picker 
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/datepicker/datepicker3.css')}}">-->
-    <!-- Daterange picker
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/daterangepicker/daterangepicker.css')}}"> -->
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet"
-          href="{{ asset('custom/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-    <!-- toaster css -->
-    <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel='stylesheet' type='text/css'>
+        <link rel="icon"  type="icon" href="{!! asset('favicon.ico') !!}">
+        <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
+        <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
+        <link rel="stylesheet" href="{!! asset('css/custom.css') !!}" />
+        <!--<link rel="stylesheet" href="http://tawian.io/text-spinners/spinners.css">-->
+        <link href="{!! asset('css/plugins/footable/footable.core.css') !!}" rel="stylesheet">
+        <link href="{!! asset('css/plugins/dataTables/datatables.min.css') !!}"
+              rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css"
+              rel="stylesheet"/>
+        <link rel="stylesheet" href="{{ asset('custom/css/responsive_style.css')}}">
+        <!-- Font Awesome -->
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+        <!-- Ionicons -->
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">  <!--roboto css-->
 
-    @yield('style')
-</head>
-<body class=" mini-navbarr">
+        <link rel="stylesheet" href="{{ asset('custom/css/responsive_style.css')}}"> <!--custom css-->
+        <link rel="stylesheet" href="{{ asset('custom/css/animate.css') }}">
+        <!--animate css-->
+        <!-- AdminLTE Skins. Choose a skin from the css/skins
+            folder instead of downloading all of them to reduce the load. -->
+        <link rel="stylesheet"
+              href="{{ asset('custom/dist/css/skins/_all-skins.min.css') }}">
+        <!-- iCheck -->
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/iCheck/flat/blue.css')}}">
+        <!-- Morris chart -->
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/morris/morris.css')}}">
+        <!-- jvectormap -->
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
+        <!-- Date Picker 
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/datepicker/datepicker3.css')}}">-->
+        <!-- Daterange picker
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/daterangepicker/daterangepicker.css')}}"> -->
+        <!-- bootstrap wysihtml5 - text editor -->
+        <link rel="stylesheet"
+              href="{{ asset('custom/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+        <!-- toaster css -->
+        <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel='stylesheet' type='text/css'>
 
-<!-- Wrapper-->
-<div id="wrapper">
+        <!--adgainer style-->
+        <link rel="stylesheet" href="{{ asset('css/adgainer.css')}}">
 
-    <!-- Navigation -->
-@include('layouts.navigation')
+        @yield('style')
+    </head>
+    <body class=" mini-navbarr">
 
-<!-- Page wraper -->
-    <div id="page-wrapper" class="gray-bg">
+        <!-- Wrapper-->
+        <div id="wrapper">
 
-        <!-- Page wrapper -->
-    @include('layouts.topnavbar')
+            <!-- Navigation -->
+            @include('layouts.navigation')
 
-    <!-- Main view animated bounceInRight -->
-        @yield('content')
-        <div id="common__modal_box" class="modal modal-fullscreen fade" role="dialog">
-            <div class="modal-dialog modal-md">
-                <div class="modal-content">
+            <!-- Page wraper -->
+            <div id="page-wrapper" class="gray-bg">
+
+                <!-- Page wrapper -->
+                @include('layouts.topnavbar')
+
+                <!-- Main view animated bounceInRight -->
+                @yield('content')
+                <div id="common__modal_box" class="modal modal-fullscreen fade" role="dialog">
+                    <div class="modal-dialog modal-md">
+                        <div class="modal-content">
+                        </div>
+                    </div>
                 </div>
+
             </div>
+            <!-- End page wrapper-->
+            <div id="Show_record"></div>
+
         </div>
-
-    </div>
-    <!-- End page wrapper-->
-    <div id="Show_record"></div>
-
-</div>
-<!-- End wrapper-->
+        <!-- End wrapper-->
 
 
-<script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
+        <script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
 
-<script src="{!! asset('js/custom_JS.js') !!}" type="text/javascript"></script>
-<script src="{!! asset('js/plugins/footable/footable.all.min.js') !!}"></script>
-<script src="{!! asset('js/plugins/dataTables/datatables.min.js')
-  !!}"></script>
-  
+        <script src="{!! asset('js/custom_JS.js') !!}" type="text/javascript"></script>
+        <script src="{!! asset('js/plugins/footable/footable.all.min.js') !!}"></script>
+        <script src="{!! asset('js/plugins/dataTables/datatables.min.js') !!}"></script>
 
-
-
-<script type="text/javascript"
-        src='https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-<script src="{{ asset('https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('//cdn.datatables.net/buttons/1.2.4/js/buttons.flash.min.js') }}"></script>
+        <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js'></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+        <script src="{{ asset('https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js') }}"></script>
+        <script src="{{ asset('//cdn.datatables.net/buttons/1.2.4/js/buttons.flash.min.js') }}"></script>
+        
+        <!--adgainer script-->
+        <script src="{{ asset('js/adgainer.js')}}"></script>
+        
 <script>
     $.fn.dialog = function(option,title=null) {
 
@@ -135,13 +137,13 @@
         }
 
     };
-    $(document).ready(function() {
+$(document).ready(function() {
 
         $('.footable').footable({
             "paging": {
                 "limit": 3,
             }
-        });
+});
 
 
     });
@@ -209,7 +211,7 @@
         });
 
     });
-</script>
+        </script>
 <script type='text/javascript'>
     $(function(){
         $("#adv_search_btn").click(function(e){
@@ -264,8 +266,8 @@
     });
 
 </script>
-@section('scripts')
-@show
+        @section('scripts')
+        @show
 
-</body>
+    </body>
 </html>
